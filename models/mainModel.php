95a6bb14666d31516f06939ffbf57614c65d9fb7
ArielBrito <arielbrit@gmail.com>
2020-08-02 18:11:03 -0400

    Creacion de Controladores

--- models/mainModel.php
+++ models/mainModel.php
@@ -47,7 +47,73 @@ class mainModel{
     }//Fin desencriptar
 
     //Metodo para generar un codigo aleatorio
-    
+    protected static function generar_codigo_aleatorio($letra, $longitud, $numero)
+    {
+        for ($i=1; $i <=$longitud ; $i++)
+        { 
+            $aleatorio=rand(0,9);
+            $letra.=$aleatorio;            
+        }
+
+        return $letra."-".$numero;        
+    }//fin aleatorio
+
+  //Metodo para limpiar cadenas de texto
+  protected static function limpiar_cadena($cadena)
+     {
+        $cadena=trim($cadena);//Eliminar espacios
+        $cadena=stripslashes($cadena);//Eliminar las flechas invertidas
+        $cadena=str_ireplace("<script>","",$cadena);//Eliminar etiquetas de javascript
+        $cadena=str_ireplace("</script>","",$cadena);
+        $cadena=str_ireplace("<script src","",$cadena);
+        $cadena=str_ireplace("<script type=","",$cadena);
+        $cadena=str_ireplace("SELECT * FROM","",$cadena);
+        $cadena=str_ireplace("DELETE * FROM","",$cadena);
+        $cadena=str_ireplace("INSERT INTO","",$cadena);
+        $cadena=str_ireplace("DROP TABLE","",$cadena);
+        $cadena=str_ireplace("DROP DATABASE","",$cadena);
+        $cadena=str_ireplace("TRUNCATE TABLE","",$cadena);
+        $cadena=str_ireplace("SHOW TABLES","",$cadena);
+        $cadena=str_ireplace("SHOW DATABASES","",$cadena);
+        $cadena=str_ireplace("<?php","",$cadena);
+        $cadena=str_ireplace("?>","",$cadena);
+        $cadena=str_ireplace("--","",$cadena);
+        $cadena=str_ireplace("<","",$cadena);
+        $cadena=str_ireplace(">","",$cadena);
+        $cadena=str_ireplace("[","",$cadena);
+        $cadena=str_ireplace("]","",$cadena);
+        $cadena=str_ireplace("^","",$cadena);
+        $cadena=str_ireplace("==","",$cadena);
+        $cadena=str_ireplace(";","",$cadena);
+        $cadena=str_ireplace("::","",$cadena);
+        $cadena=stripslashes($cadena);//Eliminar las flechas invertidas
+        $cadena=trim($cadena);//Eliminar espacios
+        return $cadena;
+     }//fin limpiar cadenas
+
+     //Metodo para validar campo
+     protected static function validar_campo($filtro, $cadena)
+     {
+         if(preg_match("/^".$filtro."$/",$cadena))
+         {
+             return false;
+         }else{
+             return true;
+         }
+     }//fin validar campo
+
+     //Metodo para validar fecha
+     protected static function validar_fecha($fecha)
+     {
+         $valores=explode("-", $fecha);
+         if(count($valores)==3 && checkdate($valores[1], $valores[2],$valores[0]))
+         {
+             return false;
+         }else{
+             return true;
+         }
+     }//fin validar fecha
+
 
 
 
