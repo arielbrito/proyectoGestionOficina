7e865afd829b0cf9bd5401ead51dbfae1ad3e4a3
ArielBrito <arielbrit@gmail.com>
2020-08-01 22:36:43 -0400

    Estructura del Proyecto

---
+++ views/inc/script.php
@@ -0,0 +1,8 @@
+<!-- jQuery -->
+<script src="<?=URL_BASE?>views/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?=URL_BASE?>views/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?=URL_BASE?>views/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?=URL_BASE?>views/js/demo.js"></script>
